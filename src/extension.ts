c7f2f4096aedf720bf94c3bbeb4305f2fe147ea1
John Ky <john@uptrust.com>
2025-06-09 08:46:09 +1000

    Underline filenames

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,48 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const decorationType = vscode.window.createTextEditorDecorationType({
+    textDecoration: 'underline',
+  });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "peek-files" is now active!');
+  const updateDecorations = (editor: vscode.TextEditor) => {
+    if (!editor) {
+      return;
+    }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('peek-files.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Peek Files!');
-	});
+    const text = editor.document.getText();
+    const fileRegex = /\b[\w\-.\/]+\.(txt|js|ts|rs|java|cpp|c|md|json|yaml|yml|py|go|rb|sh)\b/g;
+    const decorations: vscode.DecorationOptions[] = [];
 
-	context.subscriptions.push(disposable);
+    let match;
+    while ((match = fileRegex.exec(text)) !== null) {
+      const startPos = editor.document.positionAt(match.index);
+      const endPos = editor.document.positionAt(match.index + match[0].length);
+      decorations.push({ range: new vscode.Range(startPos, endPos) });
+    }
+
+    editor.setDecorations(decorationType, decorations);
+  };
+
+  // Run when an editor becomes active
+  vscode.window.onDidChangeActiveTextEditor(editor => {
+    if (editor) {
+      updateDecorations(editor);
+    }
+  }, null, context.subscriptions);
+
+  // Run on text change
+  vscode.workspace.onDidChangeTextDocument(event => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor && event.document === editor.document) {
+      updateDecorations(editor);
+    }
+  }, null, context.subscriptions);
+
+  // Run initially for active editor
+  if (vscode.window.activeTextEditor) {
+    updateDecorations(vscode.window.activeTextEditor);
+  }
 }
 
 // This method is called when your extension is deactivated
